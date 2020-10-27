53bd0f4c9d2323e8b050308bebc760c6e304f0c1
Conor Shields <conor.shields1@mydit.ie>
2020-10-27 14:34:22 +0000

    added mobile nav button

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,26 +4,43 @@ import classes from './Toolbar.module.css';
 import Auxilery from '../../../hoc/Auxilery'
 
 
-const toolbar = (props) => (
-    <Auxilery className={classes.Toolbar} >
-        <nav>
-            <div className={classes.logo}>
-                <h4>
-                    Book Select
-            </h4>
-            </div>
-            <ul className={classes.navlinks}>
-                <li>
-                    <a href="#" >New Book</a>
+class Toolbar extends React.Component {
 
-                </li>
-                <li>
-                    <a href="#" >Past Books</a>
-                </li>
-            </ul>
-        </nav>
-    </Auxilery>
+    
 
-);
 
-export default toolbar;
+    render() {
+        return (
+            <Auxilery className={classes.Toolbar} >
+                <body className={classes.body}>
+                    <nav>
+                        <div className={classes.logo}>
+                            <h4>
+                                Book Selecter
+                </h4>
+                        </div>
+                        <ul className={classes.navlinks}>
+                            <li>
+                                <a href="#" >New Book</a>
+
+                            </li>
+                            <li>
+                                <a href="#" >Past Books</a>
+                            </li>
+                        </ul>
+                        <div className={classes.burger} >
+                            <div className={classes.line1}></div>
+                            <div className={classes.line2}></div>
+                            <div className={classes.line3}></div>
+                        </div>
+                    </nav>
+                </body>
+            </Auxilery>
+        )
+    }
+}
+
+
+
+
+export default Toolbar;
