f68d87603cf749ad47c889349a47a0103780693f
Conor Shields <conor.shields1@mydit.ie>
2020-10-29 16:11:57 +0000

    adding auth0

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 
-const app  =(
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+const app = (
   <BrowserRouter>
-    <App />
+    <Auth0Provider
+      domain={domain}
+      clientId={clientID}
+      redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
+
   </BrowserRouter>
 );
 
